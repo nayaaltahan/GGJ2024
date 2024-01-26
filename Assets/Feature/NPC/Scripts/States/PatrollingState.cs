dca393121b94b153a24e0cf5447d6ec2fcf33a7c
Adam Saidane <adamsaidane@hotmail.com>
2024-01-26 22:16:38 +0100

    Animation fix + NPCs choose closest point on node to go to

--- Assets/Feature/NPC/Scripts/States/PatrollingState.cs
+++ Assets/Feature/NPC/Scripts/States/PatrollingState.cs
@@ -16,28 +16,50 @@ namespace Feature.NPC.Scripts.States
             base.OnEnterState(stateController);
             _agent = stateController.NavMeshAgent;
             _agent.isStopped = false;
-                
-            if(stateController.PatrolNodeController != null)
-                _agent.SetDestination(stateController.PatrolNodeController.PatrolNodes[_currentNode].position);
+
+
+            _currentNode = FindClosestNode(stateController);
+            _agent.SetDestination(stateController.PatrolNodeController.PatrolNodes[_currentNode].position);
         }
 
         public override void OnUpdate(NpcStateController stateController)
         {
             base.OnUpdate(stateController);
-            
+
             if (stateController.PatrolNodeController == null)
                 return;
-            
-            if (_agent.remainingDistance <= _agent.stoppingDistance)
+
+            if (_agent.remainingDistance <= _agent.stoppingDistance + 0.5f)
             {
                 _currentNode++;
                 if (_currentNode >= stateController.PatrolNodeController.PatrolNodes.Count)
                 {
                     _currentNode = 0;
                 }
+
                 _agent.SetDestination(stateController.PatrolNodeController.PatrolNodes[_currentNode].position);
             }
-            
+        }
+        
+        private int FindClosestNode(NpcStateController stateController)
+        {
+            var closestNode = 0;
+            var closestDistance = Mathf.Infinity;
+            if (stateController.PatrolNodeController != null)
+            {
+                for (var i = 0; i < stateController.PatrolNodeController.PatrolNodes.Count; i++)
+                {
+                    var distance = Vector3.Distance(stateController.PatrolNodeController.PatrolNodes[i].position,
+                        stateController.transform.position);
+                    if (distance < closestDistance)
+                    {
+                        closestDistance = distance;
+                        closestNode = i;
+                    }
+                }
+            }
+
+            return closestNode;
         }
     }
 }
