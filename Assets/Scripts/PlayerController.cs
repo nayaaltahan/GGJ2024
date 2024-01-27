05fa23cd6889622feacd67f4ceca6873a824ce90
Adam Saidane <adamsaidane252@gmail.com>
2024-01-27 12:08:20 +0100

    Add player movement (not working properly)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,17 @@
 using ScriptableObjects;
+using UnityEditor;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
     public PlayerModel playerSettings;
+    private Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
 
     void FixedUpdate()
     {
@@ -13,6 +20,30 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        GetComponent<Rigidbody>().velocity = movement * playerSettings.speed;
+        if (Input.GetKeyDown(KeyCode.Space) & IsGrounded())
+        {
+            rigidbody.AddForce(0,playerSettings.jumpForce,0, ForceMode.Impulse);
+        }
+
+        if (!IsGrounded())
+        {
+            rigidbody.AddForce(0, playerSettings.gravity,0 , ForceMode.Force);
+        }
+        
+        rigidbody.velocity = movement;
+        
+    }
+    
+    private bool IsGrounded()
+    {
+        // Check if the player is on the ground
+        return Physics.CheckSphere(transform.position, playerSettings.groundCheckDistance, playerSettings.groundLayer);
+    }
+    
+    private void OnDrawGizmos()
+    {
+        // Draw a sphere to show the ground check distance
+        Handles.color = Color.red;
+        Handles.DrawWireDisc(transform.position, Vector3.forward, playerSettings.groundCheckDistance);
     }
 }
