c98ba30345b0587e680db4e024be0077262d6a52
Adam Saidane <adamsaidane@hotmail.com>
2024-01-27 16:21:33 +0100

    Basic animations for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,27 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Transform _modelTransform;
+
     public PlayerModel playerSettings;
     private Rigidbody rigidbody;
+    private Animator _animator;
     private float _timeNotGrounded = 0f;
     private float _timeJumping = 0f;
     private bool _jumped;
+    private static readonly int MoveVelocity = Animator.StringToHash("MoveVelocity");
+    private Vector3 movement;
+    private Vector2 moveDirection;
+    private bool _isSprinting = false;
+    private static readonly int TriggerJumping = Animator.StringToHash("Trigger_Jump");
+    private static readonly int ExitJump = Animator.StringToHash("Trigger_ExitJump");
+    
 
-    void Start()
+    private
+        void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
@@ -22,17 +34,45 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) & IsGrounded())
         {
             rigidbody.velocity = new Vector3(rigidbody.velocity.x, 0f, rigidbody.velocity.z);
+            _animator.SetTrigger(TriggerJumping);
             _timeJumping = 0f;
             _jumped = true;
+        }
+        
+        _isSprinting = IsGrounded() && Input.GetKey(KeyCode.LeftShift);
+
+        
+        // set animator running speed
+        var direction = new Vector3(movement.x, 0, movement.z);
+        var moveMagnitude = direction.magnitude;
+        if (_isSprinting)
+        {
+            // Lerp moveMagnitude from current value to 2
+            moveMagnitude = Mathf.Lerp(moveMagnitude, 2f, Time.deltaTime * 10f);
+        }
+        else
+        {
+            moveMagnitude = Mathf.Clamp(moveMagnitude, 0f, 1f);
+        }
+        _animator.SetFloat(MoveVelocity, moveMagnitude);
+
+        // Rotate the model to the direction of movement
+        if (moveMagnitude > 0.1f)
+        {
+            var rotation = Quaternion.LookRotation(direction);
+            _modelTransform.rotation = Quaternion.Slerp(_modelTransform.rotation, rotation, Time.deltaTime * 10f);
         }
     }
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        moveDirection.x = Input.GetAxis("Horizontal");
+        moveDirection.y = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        movement = new Vector3(moveDirection.x, 0.0f, moveDirection.y);
+
+        if (_isSprinting)
+            movement *= 2;
 
         if (!IsGrounded())
         {
@@ -52,8 +92,11 @@ public class PlayerController : MonoBehaviour
                 _jumped = false;
             }
         }
+
         var yVelocity = playerSettings.jumpForce * playerSettings.JumpCurve.Evaluate(_timeJumping / playerSettings.AirTime);
         movement.y += yVelocity * Time.deltaTime;
+        if (_jumped == false)
+            _timeJumping = 0;
 
         rigidbody.velocity = movement;
     }
