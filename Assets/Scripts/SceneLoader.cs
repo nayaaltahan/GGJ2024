c7348d3dd1ee3fc06c365306195c375708e8c582
Adam Saidane <adamsaidane252@gmail.com>
2024-01-28 14:45:49 +0100

    Make scene loader script

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DefaultNamespace
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        public CanvasGroup MainMenu;
+        public CanvasGroup GameOverMenu;
+        
+        private void Awake()
+        {
+            MainMenu.alpha = 1;
+        }
+        
+        public void OnPlay()
+        {
+            SceneManager.LoadScene("Level", LoadSceneMode.Additive);
+            SceneManager.LoadScene("MainScene", LoadSceneMode.Additive);
+            Time.timeScale = 1;
+            MainMenu.alpha = 0;
+        }
+
+        public void OnGameOver()
+        {
+            GameOverMenu.alpha = 1;
+            Time.timeScale = 0;
+        }
+        
+        public async void OnRestart()
+        {
+            GameOverMenu.alpha = 0;
+            MainMenu.alpha = 1;
+            await SceneManager.UnloadSceneAsync("Level");
+            await SceneManager.UnloadSceneAsync("MainScene");
+        }
+
+    }
+}
