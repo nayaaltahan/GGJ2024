a73285bad5beed2a823a3ddee92cd7ea7036fbe1
Adam Saidane <adamsaidane252@gmail.com>
2024-01-28 15:20:28 +0100

    main menu

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -9,32 +9,36 @@ namespace DefaultNamespace
     {
         public CanvasGroup MainMenu;
         public CanvasGroup GameOverMenu;
+        public Camera MainMenuCamera;
         
         private void Awake()
         {
-            MainMenu.alpha = 1;
+            GameOverMenu.gameObject.SetActive(false);
+            MainMenu.gameObject.SetActive(true);
         }
         
         public void OnPlay()
         {
+            MainMenuCamera.gameObject.SetActive(false);
             SceneManager.LoadScene("Level", LoadSceneMode.Additive);
             SceneManager.LoadScene("MainScene", LoadSceneMode.Additive);
             Time.timeScale = 1;
-            MainMenu.alpha = 0;
+            MainMenu.gameObject.SetActive(false);
         }
 
         public void OnGameOver()
         {
-            GameOverMenu.alpha = 1;
+            GameOverMenu.gameObject.SetActive(true);
             Time.timeScale = 0;
         }
         
         public async void OnRestart()
         {
-            GameOverMenu.alpha = 0;
-            MainMenu.alpha = 1;
+            GameOverMenu.gameObject.SetActive(false);
+            MainMenu.gameObject.SetActive(true);
             await SceneManager.UnloadSceneAsync("Level");
             await SceneManager.UnloadSceneAsync("MainScene");
+            MainMenuCamera.gameObject.SetActive(true);
         }
 
     }
